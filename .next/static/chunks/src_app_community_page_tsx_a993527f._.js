593d6c3fa6680fe22a7e8a9af23006e2e753bd66
shobhita3 <shobhita3@gmail.com>
2025-07-04 10:03:19 +0530

    Add .next/static/chunks/src_app_community_page_tsx_a993527f._.js 1751603598764

---
+++ .next/static/chunks/src_app_community_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_c78c8d9a._.js",
+  "static/chunks/node_modules_0a1f24b4._.js"
+],
+    source: "dynamic"
+});
